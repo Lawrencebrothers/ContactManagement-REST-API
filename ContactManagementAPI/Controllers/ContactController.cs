ba672e419474d86432598adf07f223affe1e5efd
Lawrence Lawrence <31689644+Lawrencebrothers@users.noreply.github.com>
2022-04-09 14:51:07 +0100

    Add files via upload

---
+++ ContactManagementAPI/Controllers/ContactController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactManagementAPI.Controllers;
+using ContactManagementAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using ContactManagementAPI.Services;
+
+
+namespace ContactManagementAPI.Controllers
+    {
+
+   [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        public ContactController()
+        {
+            
+            
+        }
+       
+[HttpGet]
+        public ActionResult<List<ContactDirectory>> GetAll()
+        {
+          return  contactService.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ContactDirectory> Get(int id)
+        {
+            var contact = contactService.Get(id);
+            if (contact == null)
+            
+                return NotFound();
+
+           
+            return contact;
+
+        }
+        [HttpPost]
+        public IActionResult Create(ContactDirectory contact
+            )
+        {
+            contactService.Add(contact);
+            return CreatedAtAction(nameof(Create), new { id = contact.Id }, contact);
+        }
+
+       [ HttpPut("{id}")]
+public IActionResult Update(int id, ContactDirectory contact)
+        {
+            if (id != contact.Id)
+                return BadRequest();
+
+            var existingContact = contactService.Get(id);
+            if (existingContact is null)
+                return NotFound();
+
+            contactService.Update(contact);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var contact = contactService.Get(id);
+
+            if (contact is null)
+                return NotFound();
+
+           contactService.Delete(id);
+
+            return NoContent();
+        }
+
+    }
+
+}
+
